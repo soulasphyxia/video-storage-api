c33d253e073602a3e925b2d921691e475aa8f57a
Vitaly Maslyuk <maslyuk03@bk.ru>
2024-06-17 01:24:28 +0700

    commit

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,10 @@ RUN mvn -f /app/pom.xml clean package -Dmaven.test.skip=true
 FROM eclipse-temurin:17-jre-alpine
 WORKDIR /app
 COPY --from=stage1 /app/target/*.jar /app/*.jar
+COPY --from=stage1 /app/ffmpeg_script.sh /app/ffmpeg_script.sh
 RUN apk update
 RUN apk upgrade
 RUN apk add --no-cache ffmpeg
+RUN apk add --no-cache bash
 EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "/app/*.jar"]
--- src/main/java/soulasphyxia/videostorageapi/controllers/HashTagController.java
+++ src/main/java/soulasphyxia/videostorageapi/controllers/HashTagController.java
@@ -3,6 +3,7 @@ package soulasphyxia.videostorageapi.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import soulasphyxia.videostorageapi.services.HashTagService;
 @RestController
 @RequestMapping("/api/v1/hashtags")
 @RequiredArgsConstructor
+@CrossOrigin
 public class HashTagController {
     private final HashTagService hashTagService;
     @GetMapping
